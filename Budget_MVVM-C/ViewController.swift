ee6eabc360e083c555e5c19dae4c7ede782907d6
DwightShrut <skatemax@mail.ru>
2025-10-24 18:56:21 +0300

    Initial Commit

---
+++ Budget_MVVM-C/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Budget_MVVM-C
+//
+//  Created by Макс Макеев on 24.10.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
