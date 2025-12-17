eb2471bee3ec25cedff63c0ad5a9fe62d67e09ee
DwightShrut <skatemax@mail.ru>
2025-12-17 23:21:21 +0300

    create Budget Page ViewController

---
+++ Budget_MVVM-C/Sources/Modules/Budget/BudgetViewController.swift
@@ -0,0 +1,15 @@
+//
+//  BudgetViewController.swift
+//  Budget_MVVM-C
+//
+//  Created by Макс Макеев on 07.11.2025.
+//
+
+import UIKit
+
+final class BudgetViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .cyan
+    }
+}
